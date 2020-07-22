30c0875efb6c6c5c65e055730d1114e2f3786ea4
Tom van den Berg <tom@berg-vanden.nl>
2020-07-22 17:14:21 +0200

    Adding Timer2 library for servo

--- Servotester/Servotester.ino
+++ Servotester/Servotester.ino
@@ -2,27 +2,34 @@
   LiquidCrystal Library
   modified from liquid Crystal example.
 
-  http://www.arduino.cc/en/Tutorial/LiquidCrystal
+  https://www.arduino.cc/en/Tutorial/LiquidCrystal
+
+  Servo timer 2 library from https://github.com/nabontra/ServoTimer2
+  Guide by https://www.hackster.io/ashraf_minhaj/how-to-use-servotimer2-library-simple-explain-servo-sweep-512fd9
 
   Modified for Servo by https://www.buildcomics.com
+  
+  Wiring & Latest code: https://github.com/buildcomics/arduino-hero-toolbox
 */
-#define SERVOPIN 11
+//Define our servopin number here (2 is used here, it allows to connect the servo connector directly to the bottom left of the 2x5 pin default header, see https://github.com/buildcomics/arduino-hero-toolbox
+#define SERVOPIN 2
 
 #include <LiquidCrystal.h> // include the library code for the display
+#include <ServoTimer2.h>  // the servo library
 
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);           // select the pins used on the LCD panel
-unsigned long int previousMillis;
-const unsigned int interval = 100;
-unsigned int pwmVal;
-
+unsigned long int previousMillis; //Holds previous millis for updating time
+const unsigned int interval = 100; //This is our interval for updating the screen
+unsigned int pwmVal; //This value holds our set pwm value
+ServoTimer2 servo; //The servo object
 
 void setup() {
   lcd.begin(16, 2);   // set up the LCD's number of columns and rows:
-  pwmVal = 100;
+  pwmVal = 90; //Initializing the pwm value to 90 degrees
 
-  pinMode(SERVOPIN, OUTPUT); //OC2A output is D11
-
+  servo.attach(SERVOPIN); //Attach servo timer 2 object to SERVOPIN
+    
   //init message:
   lcd.print("I am a HERO!(BC)");
   lcd.setCursor(0, 1);
@@ -41,15 +48,14 @@ void loop() {
   if (timeDif >= interval) {
     previousMillis = currentMillis; //reset counter
     unsigned int x;
-
     x = analogRead(A0); //read buttons
     if (x < 60) { //right button
-      if (pwmVal < 231) {
+      if (pwmVal <= 170) {
         pwmVal += 10;
       }
     }
     else if (x < 200) { //up button
-      if (pwmVal < 240) {
+      if (pwmVal < 180) {
         pwmVal++;
       }
     }
@@ -72,10 +78,5 @@ void loop() {
     lcd.print(pwmVal);
     lcd.print("  "); //clear excess characters
   }
-  digitalWrite(SERVOPIN, HIGH);
-  delayMicroseconds(800); //5*180=900 microseconds
-  delayMicroseconds(pwmVal*7); //5*180=900 microseconds
-  digitalWrite(SERVOPIN, LOW);
-  delayMicroseconds(1200-(pwmVal*7)); //fill up to one millisecond
-  delay(18); //Complete 20ms period  
+  servo.write(map(pwmVal, 0, 180, 750, 2250)); //Map values 0 to 180 to pwm timer 1 values and write
 }
